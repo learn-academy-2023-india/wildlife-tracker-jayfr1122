df8cf97944abd17ba86d64a894419523d8e01a0a
jayfr1122 <jayfranks21@gmail.com>
2024-02-01 13:30:48 -0800

    finishing story 2

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -11,9 +11,29 @@ class SightingsController < ApplicationController
             render json: sighting.errors
         end
     end
+    def destroy
+        sighting = sighting.find(params[:id])
+        if sighting.destroy
+            render json: sighting
+        else
+            render json: sighting.errors
+        end
+    end
+
+    def update
+        sighting = sighting.find(params[:id])
+        sighting.update(sighting)
+        if sighting.valid?
+            render json: sighting
+        else
+            render json: sighting.errors
+        end
+    end
+
+
     private
     def sighting_params
         params.require(:sighting).permit(:animal_id, :latitude, :longitude, :date)
-        # make sure to have all 
+        # make sure to have all key symbols referenced when creating new entries, or params won't be met.
     end
 end
